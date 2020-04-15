3537d390ebfc324ad56be038fc87b0113c81af71
magicandcode <magicandcode@gmail.com>
2020-04-15 20:09:09 +0200

    Group coding meetup #1

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -30,28 +30,50 @@ def game(words: List[str], game_count: int):
 
     # Get a solution to match player's guesses against.
     solution: str = get_game_solution(words)
-    solution_chars: List[str] = []
-    result_chars: List[str] = []
-    result: str = ''
+    # solution_chars: List[str] = []
+    # solution = 'APPLE'
+    result_chars: List[str] = ['_' for _ in solution]
+    # ['_', '_', '_', '_', '_']
+    result: str = ''.join(result_chars)
+    # '_____' == 'APPLE'  #False
+    # '_PP__' == 'APPLE'  # False
+    # 'APPLE' == 'APPLE'  # True
 
+    '''
+     O
+    \|/
+     |
+    / \
+    ''' 
+     
+    solution[0] == 'A'
+    solution[-1] == 'E'
+
+    print(solution)
+    
     # Loop game until player wins or loses.
+    while guess_count <= MAX_GUESS_COUNT:
+      # Ask user to guess a letter.  Guess must be converted to upper to
+      #  match against solution.
+      guess = input('Guess a letter (A-Z): ').upper()
+      
+      # Check if guess is in solution.
+      breakpoint()
+      # 'A' in 'REASON'
+      if guess in solution:
+        print("correct")
+      else:
+        print("incorrect")
+      # If guess is correct, replace empty slots with correct guess in
+      #   result and result_chars and display message.
 
-    # Ask user to guess a letter.  Guess must be converted to upper to
-    #  match against solution.
-    # guess = input('Guess a letter (A-Z): ').upper()
+      # If guess is wrong, decrement guess_count and display message.
 
+      # Has player won or lost?
 
-    # Check if guess is in solution.
-
-    # If guess is correct, replace empty slots with correct guess in
-    #   result and result_chars and display message.
-
-    # If guess is wrong, decrement guess_count and display message.
-
-    # Has player won or lost?
-
+    guess_count += 1
     # Break out of game loop if player wins or loses.
 
 
 if __name__ == '__main__':
-    play(game)
+  play(game)
