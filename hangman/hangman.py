8af2376389e77760181a1fb58048809d6aee53a6
magicandcode <magicandcode@gmail.com>
2020-04-20 22:32:44 +0200

    Refactor and add additional comments

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,7 +1,13 @@
 from typing import List, Optional
 import random
 
-from game_utils import get_words, get_game_solution, play
+from game_helpers import (get_words,
+                          get_game_solution,
+                          get_valid_guess,
+                          play,
+                          player_wins,
+                         )
+
 
 # Seed to make our randomness consistent during development.
 random.seed(42)
@@ -24,54 +30,56 @@ def game(words: List[str], game_count: int):
     # Print initial game message.
     print(f'\nStarting Game #{game_count}')
 
-    # Game settings.
+    # Initial game settings.
     MAX_GUESS_COUNT: int = 5
     guess_count: int = 0
 
     # Get a solution to match player's guesses against.
     solution: str = get_game_solution(words)
-    # solution_chars: List[str] = []
-    # solution = 'APPLE'
-    result_chars: List[str] = ['_' for _ in solution]
+
+    # Initial result will be a string with n _ where n is the length
+    #   of the solution string.
+    # To enable replacing _ with guess when correct, we create a list
+    #   with n number of _
     # ['_', '_', '_', '_', '_']
-    result: str = ''.join(result_chars)
-    # '_____' == 'APPLE'  #False
+    result_chars: List[str] = ['_' for _ in solution]
+    
+    # The list can then be joined to create our result string which is
+    #   compared to the solution to check if player wins.
+    # '_____'
     # '_PP__' == 'APPLE'  # False
     # 'APPLE' == 'APPLE'  # True
+    result: str = ''.join(result_chars)
 
-    '''
-     O
-    \|/
-     |
-    / \
-    ''' 
-     
-    solution[0] == 'A'
-    solution[-1] == 'E'
 
-    print(solution)
-    
     # Loop game until player wins or loses.
+    # We exit the loop when the player wins, or when the number of
+    #   (incorrect) guesses is greater than the max number of guesses.
+    # Alternatively, we can run the game loop infintely as long as we
+    #   break out of it when the player wins or loses.
+    # We can also set guess_count to 5 and decrement with each
+    #   incorrect guess.  That gives us the more explicit expression:
+    #       while guesses_remaining:
+    # Once guesses_remaining equals 0 the expression is False and we
+    #   exit the loop.
     while guess_count <= MAX_GUESS_COUNT:
-      # Ask user to guess a letter.  Guess must be converted to upper to
-      #  match against solution.
-      guess = input('Guess a letter (A-Z): ').upper()
-      
-      # Check if guess is in solution.
-      breakpoint()
-      # 'A' in 'REASON'
-      if guess in solution:
-        print("correct")
-      else:
-        print("incorrect")
-      # If guess is correct, replace empty slots with correct guess in
-      #   result and result_chars and display message.
+      # Ask user to guess a letter.  Validate guess.  Reprompt player
+      #   until guess is valid.
+      guess: str = get_valid_guess()
 
-      # If guess is wrong, decrement guess_count and display message.
+      # Check if guess is a character in the solution.
+      if guess in solution:
+        # If guess is correct, replace empty slots with correct guess in
+        #   result and result_chars and display message.
+        print("correct")
+        break
+      else:
+        # If guess is wrong, increment guess_count and display message.
+        guess_count += 1
+        print("incorrect")
 
       # Has player won or lost?
 
-    guess_count += 1
     # Break out of game loop if player wins or loses.
 
 
